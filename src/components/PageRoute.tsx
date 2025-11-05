513cc8f777cd316e17c3936ab9fb741a3ee61555
Aphilos <sean.moore.gonzalez@gmail.com>
2025-11-04 17:23:37 -0800

    PageSponsor is using backend calls

--- src/components/PageRoute.tsx
+++ src/components/PageRoute.tsx
@@ -16,7 +16,7 @@ type WalletType = {
 function getPageForWallet(wallet: WalletType) {
   // if (wallet?.address === '0x338DE89f3BB60444BAc39D27dDd8324A2497Cb8f') {
   // return <PageUser />;
-  return <PageHost eventShortId={'SMu78xNR'} />;
+  // return <PageHost eventShortId={'SMu78xNR'} />;
   // return <PageHost walletId={'6900cb85d97bec334b6b5902'} />;
   // }
   // if (wallet?.address === '0x338DE89f3BB60444BAc39D27dDd8324A2497Cb8f') {
